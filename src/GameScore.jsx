914215726fcbb4d9fb4dd949bfa3d627718c188a
Younes-aouka <youneseaouka@gmail.com>
2025-07-23 14:46:00 +0100

    score to green

--- src/GameScore.jsx
+++ src/GameScore.jsx
@@ -4,7 +4,7 @@ const GameScore = () => {
     const gameScore = useStore((states) => states.gameScore);
     return (
         <div className="absolute top-0 left-0 flex flex-col items-center px-1 py-1 text-center">
-            <h3 className="  font-bold text-xl md:text-2xl text-white">Score: {gameScore}</h3>
+            <h3 className="  font-bold text-xl md:text-2xl text-[#1ebc1e]">Score: {gameScore}</h3>
             <div className="md:font-semibold text-white text-sm md:text-xl">
                 <div>&larr; left</div>
                 <div>&rarr; right</div>
